0b94cf1ae5b05a7c6ff28121b9a8c770cceb1bfc
sesaretah <hs.shafiei@gmail.com>
2020-10-05 22:40:16 +0330

    1

---
+++ public/precache-manifest.3ee5b468736cb8005b11f3aafa71a050.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae7c3b4b9b1117e8abf2f546d554e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22636125cdd4507b95e1",
+    "url": "/static/css/main.32458517.chunk.css"
+  },
+  {
+    "revision": "a4afedfcf7de8670fb81",
+    "url": "/static/js/2.0cc76eb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0cc76eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22636125cdd4507b95e1",
+    "url": "/static/js/main.c7cff36b.chunk.js"
+  },
+  {
+    "revision": "847ed76729da8954a7b5",
+    "url": "/static/js/runtime-main.e5453d19.js"
+  },
+  {
+    "revision": "07e13ce12b31374a49045aa55af2c0ad",
+    "url": "/static/media/IRANSansWeb.07e13ce1.eot"
+  },
+  {
+    "revision": "a5080d9e55b71a50ecd7b7d7ad022754",
+    "url": "/static/media/IRANSansWeb.a5080d9e.ttf"
+  },
+  {
+    "revision": "d4bb655d07591559e417030519d6fa20",
+    "url": "/static/media/IRANSansWeb.d4bb655d.woff"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2c5503a10e2e7deb06e0d370275f3d23.js"
+  "/precache-manifest.3ee5b468736cb8005b11f3aafa71a050.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
